4cc9ba781e6401c1907c7c270b38726040944960
pavlovsky64 <81993869+pavlovsky64@users.noreply.github.com>
2023-09-11 21:42:58 -0400

    Tiempo
    parte 3

---
+++ time.js
@@ -0,0 +1,15 @@
+var now = new Date();
+
+function timing(){
+    const horaActual = [now.getHours(), now.getMinutes()];
+    const randomMinute = (Math.floor(Math.random()*300)+300);//en 5 horas hay 300 minutos
+    const horasFinales= Math.floor(horaActual[0]+(randomMinute+horaActual[1])/60)%24;
+    const minutosFinales= (randomMinute+horaActual[1])%60
+
+    const horaFinal = [horasFinales, minutosFinales];
+
+    console.log(`hora inicial: ${horaActual[0]}:${horaActual[1]}`);
+    console.log("valor aleatoreo en minutos:", randomMinute);
+    console.log(`hora final: ${horaFinal[0]}:${horaFinal[1]}`);
+}
+timing();
